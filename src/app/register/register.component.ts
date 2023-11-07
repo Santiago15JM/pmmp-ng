a21e9538a273ba776e7b4c6af8ff32d94d4a2ffa
cristian1clj <cristian1clj@gmail.com>
2023-11-07 10:14:31 -0500

    Created Register component

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+import { RegisterUser } from 'src/models/user.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  user: RegisterUser = {
+    name: '',
+    lastname: '',
+    email: '',
+    phone: 0,
+    address: ''
+  }
+
+  constructor(private router: Router, private Api: ApiService){}
+
+  onSubmit(user: RegisterUser) {
+    this.Api.createUser(user);
+    this.router.navigate(['']);
+  }
+}
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -7,6 +7,8 @@ export interface User {
   address: string
 }
 
+export interface RegisterUser extends Omit<User, "id"> { }
+
 export interface LoginUser extends Omit<User, "id"|"name"|"lastname"|"phone"|"address"> {
   password: string
 }
